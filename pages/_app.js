c1c8b11e56371b8efcc96204afef87c0b56c2a97
Weston Nelson <dogooddowell@protonmail.com>
2023-04-28 23:27:28 +0000

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,30 @@
-import { ThirdwebSDK } from "@thirdweb-dev/sdk/evm";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
 
-const sdk = new ThirdwebSDK("arbitrum");
-const contract = await sdk.getContract("0xaA9D9a8f810463f24825c05f3075c1Fc6a430430");
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "arbitrum";
 
-const appURI = await contract.appURI.get(appURI);
-console.log(appURI) // "ipfs://some_ipfs_hash";
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider activeChain={activeChain}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
 
-const metadata = await contract.metadata.get();
-console.log(metadata);
+import { Web3Button } from "@thirdweb-dev/react";
 
-const rolesAndMembers = await contract.roles.getAll();
+const Home = () => {
+  return (
+    <Web3Button
+      contractAddress="{{contract_address}}"
+      action={async (contract) => contract.call("myFunctionName")}
+    >
+      Call myFunctionName from the connected wallet
+    </Web3Button>
+  );
+};
+
+export default MyApp;
